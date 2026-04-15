81c8fd4c7192c42952411152bc844df1cbef7bff
th0truth <shiftrams@gmail.com>
2026-04-15 21:41:20 +0300

    feat: add repl

--- src/nanokv.cpp
+++ src/nanokv.cpp
@@ -7,92 +7,12 @@
 #include "doubly_linked_list.h"
 #include "circular_linked_list.h"
 #include "stack.h"
+#include "repl.h"
 
 using namespace std;
 
-template <typename T>
-void run_repl(T& db) {
-  string command, key, value;
-
-  cout << "=== NanoKV ===\n";
-  cout << "Commands: SET <key> <value> | GET <key> | DEL <key> | DISPLAY | EXIT\n\n";
-
-  while (true) {
-    cout << "db> ";
-    if (!(cin >> command)) break;
-
-    if (command == "EXIT" || command == "exit") {
-      break;
-    } else if (command == "SET" || command == "set") {
-      cin >> key;
-      getline(cin >> ws, value);
-      db.insert(key, value);
-      cout << "OK\n";
-    } else if (command == "GET" || command == "get") {
-      cin >> key;
-      string *result = db.search(key);
-      if (result) {
-        cout << "\"" << *result << "\"\n";
-      } else {
-        cout << "(null)\n";
-      }
-    } else if (command == "DEL" || command == "del") {
-      cin >> key;
-      db.remove(key);
-      cout << "OK\n";
-    } else if (command == "DISPLAY" || command == "display") {
-      db.display("Current State: ");
-    } else {
-      cout << "Error: Unknown command '" << command << "'\n";
-      cin.ignore(10000, '\n');  // clear line
-    }
-  }
-}
-
-template <typename T>
-void run_stack_repl(Stack<T>& stack) {
-  string command, value;
-
-  cout << "=== NanoKV (Stack Mode) ===\n";
-  cout << "Commands: PUSH <value> | POP | TOP | SIZE | DISPLAY | EXIT\n\n";
-
-  while (true) {
-    cout << "stack> ";
-    if (!(cin >> command)) break;
-
-    if (command == "EXIT" || command == "exit") {
-      break;
-    } else if (command == "PUSH" || command == "push") {
-      cin >> value;
-      if (stack.push(value)) {
-        cout << "OK\n";
-      } else {
-        cout << "Error: Stack is full\n";
-      }
-    } else if (command == "POP" || command == "pop") {
-      if (stack.pop()) {
-        cout << "OK\n";
-      } else {
-        cout << "Error: Stack is empty\n";
-      }
-    } else if (command == "TOP" || command == "top") {
-      if (!stack.isEmpty()) {
-        cout << "\"" << stack.top() << "\"\n";
-      } else {
-        cout << "(empty)\n";
-      }
-    } else if (command == "SIZE" || command == "size") {
-      cout << stack.size() << " / " << stack.getCapacity() << "\n";
-    } else if (command == "DISPLAY" || command == "display") {
-      stack.display("Current Stack State:");
-    } else {
-      cout << "Error: Unknown command '" << command << "'\n";
-      cin.ignore(10000, '\n');  // clear line
-    }
-  }
-}
-
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   ProbingStrategy strategy = ProbingStrategy::DOUBLE_HASHING; 
   bool use_chaining = false;
   bool use_list = false;
